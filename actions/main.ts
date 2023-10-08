b83f8849cfe109d5acc19ed03baeefee6043e65b
Cong Thanh Vu <congthanh3181@gmail.com>
2023-10-08 18:58:19 +0700

    change source dir

--- actions/main.ts
+++ actions/main.ts
@@ -5,7 +5,7 @@ async function run(): Promise<void> {
     try {
         info("Cloning deployment kit into the board");
         subProcess.execSync(`git clone --branch ${process.env.SVLD_VERSION as string} https://github.com/cngthnh/serverless-mephisto .deploy`)
-        subProcess.execSync("mkdir -p ../.deploy/app_src && rsync -a --exclude=../.deploy ../ ../.deploy/app_src");
+        subProcess.execSync("mkdir -p ./.deploy/app_src && rsync -a --exclude=./.deploy ./ ./.deploy/app_src");
         
         info("Signing in ECR");
         let buffer = subProcess.execSync(`aws ecr get-login-password --region ${process.env.AWS_REGION as string} | docker login --username AWS ` +
@@ -16,7 +16,7 @@ async function run(): Promise<void> {
         buffer = subProcess.execSync(`cd .deploy && npm install`);
         info(buffer.toString());
         info("Deploying...")
-        let stream = subProcess.exec(`cd .deploy && ls . && npm run deploy`);
+        let stream = subProcess.exec(`cd .deploy && ls app_src . && npm run deploy`);
         stream.stdout?.on('data', (data) => {
             info(data);
         });
