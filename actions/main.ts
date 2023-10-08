f08dc0f8a80d52ebbcaaabdd389e028cb6cb9ccf
Cong Thanh Vu <congthanh3181@gmail.com>
2023-10-08 17:58:52 +0700

    test gh workspace

--- actions/main.ts
+++ actions/main.ts
@@ -3,14 +3,12 @@ import * as subProcess from 'child_process';
 
 async function run(): Promise<void> {
     try {
-        subProcess.exec(`ls ${process.env.GITHUB_WORKSPACE as string}`, (err, stdout, stderr) => {
-            info("GH Workspace:");
-            info(stdout);
-            info(stderr);
-        });
-        subProcess.exec(`aws ecr get-login-password --region ${process.env.AWS_REGION as string} | docker login --username AWS ` +
+        info("GH Workspace ENV: " + process.env.GITHUB_WORKSPACE as string)
+        const buffer = subProcess.execSync(`ls ${process.env.GITHUB_WORKSPACE as string}`);
+        info(buffer.toString());
+        subProcess.execSync(`aws ecr get-login-password --region ${process.env.AWS_REGION as string} | docker login --username AWS ` +
             `--password-stdin ${process.env.AWS_ACCOUNT_ID as string}.dkr.ecr.${process.env.AWS_REGION as string}.amazonaws.com`);
-        subProcess.exec(`npm run deploy`);
+        subProcess.execSync(`npm run deploy`);
     } catch (e: any) {
         setFailed(e);
     }
