02b47529ba032dd6f589865a1dddc44e4f85a6f1
Cong Thanh Vu <congthanh3181@gmail.com>
2023-10-08 18:22:06 +0700

    clone deployment kit into the working dir

--- actions/main.ts
+++ actions/main.ts
@@ -3,12 +3,18 @@ import * as subProcess from 'child_process';
 
 async function run(): Promise<void> {
     try {
-        const buffer = subProcess.execSync("pwd");
-        info("Current working dir: " + buffer.toString());
-        subProcess.execSync(`cp -r ${process.env.GITHUB_WORKSPACE as string}/* app_src/`);
-        subProcess.execSync(`aws ecr get-login-password --region ${process.env.AWS_REGION as string} | docker login --username AWS ` +
+        info("Cloning deployment kit into the board");
+        subProcess.execSync(`git clone --branch ${process.env.SVLD_VERSION as string} https://github.com/cngthnh/serverless-mephisto .deploy`)
+        subProcess.execSync("mkdir -p ../.deploy/app_src && rsync -a --exclude=../.deploy ../ ../.deploy/app_src");
+        
+        info("Signing in ECR");
+        let buffer = subProcess.execSync(`aws ecr get-login-password --region ${process.env.AWS_REGION as string} | docker login --username AWS ` +
             `--password-stdin ${process.env.AWS_ACCOUNT_ID as string}.dkr.ecr.${process.env.AWS_REGION as string}.amazonaws.com`);
-        subProcess.execSync(`npm run deploy`);
+        info(buffer.toString());
+
+        info("Deploying...");
+        buffer = subProcess.execSync(`npm run deploy`);
+        info(buffer.toString());
     } catch (e: any) {
         setFailed(e);
     }
