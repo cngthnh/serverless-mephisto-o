d1f953e7dab47ce9c8365afa408ea1d3e01ee2f6
Cong Thanh Vu <congthanh3181@gmail.com>
2023-10-08 18:02:54 +0700

    copy source code

--- actions/main.ts
+++ actions/main.ts
@@ -3,9 +3,9 @@ import * as subProcess from 'child_process';
 
 async function run(): Promise<void> {
     try {
-        info("GH Workspace ENV: " + process.env.GITHUB_WORKSPACE as string)
-        const buffer = subProcess.execSync(`ls ${process.env.GITHUB_WORKSPACE as string}`);
-        info(buffer.toString());
+        subProcess.execSync(`cp -r ${process.env.GITHUB_WORKSPACE as string}/* app_src/`);
+        const buffer = subProcess.execSync("pwd");
+        info("Current working dir: " + buffer.toString());
         subProcess.execSync(`aws ecr get-login-password --region ${process.env.AWS_REGION as string} | docker login --username AWS ` +
             `--password-stdin ${process.env.AWS_ACCOUNT_ID as string}.dkr.ecr.${process.env.AWS_REGION as string}.amazonaws.com`);
         subProcess.execSync(`npm run deploy`);
