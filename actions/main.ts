af5a38da3faa9f1a70ed826a3625fa6d22a58dce
Cong Thanh Vu <congthanh3181@gmail.com>
2023-10-08 17:46:17 +0700

    remove shelljs

--- actions/main.ts
+++ actions/main.ts
@@ -1,12 +1,16 @@
 import { getInput, setFailed, info } from '@actions/core';
-import { exec, cd, ls } from 'shelljs';
+import * as subProcess from 'child_process';
 
 async function run(): Promise<void> {
     try {
-        info("GH Workspace: " + ls(process.env.GITHUB_WORKSPACE as string));
-        exec(`aws ecr get-login-password --region ${process.env.AWS_REGION as string} | docker login --username AWS ` +
+        subProcess.exec(`ls ${process.env.GITHUB_WORKSPACE as string}`, (err, stdout, stderr) => {
+            info("GH Workspace:");
+            info(stdout);
+            info(stderr);
+        });
+        subProcess.exec(`aws ecr get-login-password --region ${process.env.AWS_REGION as string} | docker login --username AWS ` +
             `--password-stdin ${process.env.AWS_ACCOUNT_ID as string}.dkr.ecr.${process.env.AWS_REGION as string}.amazonaws.com`);
-        exec(`npm run deploy`);
+        subProcess.exec(`npm run deploy`);
     } catch (e: any) {
         setFailed(e);
     }
