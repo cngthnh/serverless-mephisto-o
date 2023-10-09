60c8391c7ebd81c0fc8ce5108930dc2e8f61d8cc
Cong Thanh Vu <congthanh3181@gmail.com>
2023-10-10 00:29:41 +0700

    add tags for stack

--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,16 @@
 import { SSTConfig } from "sst";
+import { Tags } from "aws-cdk-lib";
 import { DefaultStack } from "./stacks/DefaultStack";
 
 export default {
   config(_input) {
     return {
       name: `serverless-mephisto-${process.env.APP_NAME}-${process.env.APP_ENV}`,
-      region: process.env.AWS_REGION || "ap-southeast-2",
+      region: process.env.AWS_REGION || "ap-southeast-2"
     };
   },
   stacks(app) {
+    Tags.of(app).add("task-tag", `${app.name}-${app.stage}`);
     app.stack(DefaultStack);
   },
 } satisfies SSTConfig;
