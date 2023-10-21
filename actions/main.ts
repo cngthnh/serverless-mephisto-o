6038435296bd87195c26d4a62984f0cb5ff45bca
Cong Thanh Vu <congthanh3181@gmail.com>
2023-10-21 22:11:10 +0700

    test

--- actions/main.ts
+++ actions/main.ts
@@ -13,16 +13,9 @@ async function run(): Promise<void> {
         info(buffer.toString());
 
         info("Waiting for confirmation...");
-        const stream = subProcess.exec(`aws logs tail /sst/service/dev-serverless-mephisto-task-test-deployment-test-dev-wvpc-mephisto-task-test-deployment-test-dev-wvpc	--follow --since 1d`);
-        info("STREAM")
-        stream.stdout?.on('data', (data) => {
-            info(data);
-        });
-        stream.stderr?.on('data', (data) => {
-            info("stderr: " + data);
-        });
+        subProcess.execSync(`aws logs tail /sst/service/dev-serverless-mephisto-task-test-deployment-test-dev-wvpc-mephisto-task-test-deployment-test-dev-wvpc --follow --since 1d | egrep '${process.env.PREVIEW_URL_PREFIX}' > _run.log`);
         subProcess.execSync("cat _run.log")
-        buffer = subProcess.execSync(`while ! grep '${process.env.PREVIEW_URL_PREFIX}' _run.log; do sleep 1; done`);
+        buffer = subProcess.execSync(`while ! grep '${process.env.PREVIEW_URL_PREFIX}' _run.log; do cat _run.log; sleep 1; done`);
         buffer = subProcess.execSync(`grep '${process.env.PREVIEW_URL_PREFIX}' _run.log`);
         info(buffer.toString());
 
