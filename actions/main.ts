11296fb6aeb7ce210fac5f7e62eb8d50fdb66925
Cong Thanh Vu <congthanh3181@gmail.com>
2023-10-22 12:04:01 +0700

    test

--- actions/main.ts
+++ actions/main.ts
@@ -11,40 +11,49 @@ async function run(): Promise<void> {
         let buffer = subProcess.execSync(`aws ecr get-login-password --region ${process.env.AWS_REGION as string} | docker login --username AWS ` +
             `--password-stdin ${process.env.AWS_ACCOUNT_ID as string}.dkr.ecr.${process.env.AWS_REGION as string}.amazonaws.com`);
         info(buffer.toString());
-
-        info("Waiting for confirmation...");
         
-        const grepPattern = process.env.PREVIEW_URL_PREFIX?.slice(1,-1);
-        buffer = subProcess.execSync(`while ! aws logs tail /sst/service/dev-serverless-mephisto-task-test-deployment-test-dev-wvpc-mephisto-task-test-deployment-test-dev-wvpc --filter-pattern '${process.env.PREVIEW_URL_PREFIX}' --since 1d | grep '${grepPattern}'; do sleep 5; done`);
+        info("Installing dependencies...");
+        buffer = subProcess.execSync(`cd .deploy && npm install`);
         info(buffer.toString());
+
+        // info("Removing old stacks");
+        // buffer = subProcess.execSync(`cd .deploy && echo "${process.env.APP_ENV}" | npm run remove`);
+        // info(buffer.toString());
+
+        const repoName = `${process.env.APP_NAME}-${process.env.APP_ENV}`;
+        info("Creating repository...");
+        buffer = subProcess.execSync(`cd .deploy && aws ecr create-repository --repository-name "${repoName}" --region ${process.env.AWS_REGION} || true`);
+        info(buffer.toString());
+
+        info("Putting lifecycle policy...");
+        buffer = subProcess.execSync(`cd .deploy && aws ecr put-lifecycle-policy --repository-name "${repoName}" --lifecycle-policy-text file://$(pwd)/conf/lifecycle_policy.json || true`);
+        info(buffer.toString());
+
+        info("Deploying...");
+
+        const startTime = new Date().getTime();
+
+        let stream = subProcess.exec(`cd .deploy && echo "${process.env.APP_ENV}" | npm run deploy`);
+        stream.stdout?.on('data', (data) => {
+            info(data);
+        });
+        stream.stderr?.on('data', (data) => {
+            info("stderr: " + data);
+        });
+
+        stream.on('exit', () => {
+            info("Waiting for confirmation...");
+
+            const execTime = Math.ceil((new Date().getTime() - startTime) / 60000);
+            info(`Deployment process time: ${execTime} minutes`);
+
+            const grepPattern = process.env.PREVIEW_URL_PREFIX?.slice(1,-1);
+            buffer = subProcess.execSync(`while ! aws logs tail /sst/service/${process.env.APP_ENV}-serverless-${process.env.APP_NAME}-${process.env.APP_ENV}-${process.env.APP_NAME}-${process.env.APP_ENV} --filter-pattern '${process.env.PREVIEW_URL_PREFIX}' --since ${execTime}m | grep '${grepPattern}'; do sleep 5; done`);
+            
+            buffer = subProcess.execSync(`aws logs tail /sst/service/${process.env.APP_ENV}-serverless-${process.env.APP_NAME}-${process.env.APP_ENV}-${process.env.APP_NAME}-${process.env.APP_ENV} --filter-pattern '${process.env.PREVIEW_URL_PREFIX}' --since ${execTime}m`)
+            info(buffer.toString());
+        });
         
-        buffer = subProcess.execSync(`aws logs tail /sst/service/dev-serverless-mephisto-task-test-deployment-test-dev-wvpc-mephisto-task-test-deployment-test-dev-wvpc --filter-pattern '${process.env.PREVIEW_URL_PREFIX}' --since 1d`)
-        info(buffer.toString());
-        // info("Installing dependencies...");
-        // buffer = subProcess.execSync(`cd .deploy && npm install`);
-        // info(buffer.toString());
-
-        // // info("Removing old stacks");
-        // // buffer = subProcess.execSync(`cd .deploy && echo "${process.env.APP_ENV}" | npm run remove`);
-        // // info(buffer.toString());
-
-        // const repoName = `${process.env.APP_NAME}-${process.env.APP_ENV}`;
-        // info("Creating repository...");
-        // buffer = subProcess.execSync(`cd .deploy && aws ecr create-repository --repository-name "${repoName}" --region ${process.env.AWS_REGION} || true`);
-        // info(buffer.toString());
-
-        // info("Putting lifecycle policy...");
-        // buffer = subProcess.execSync(`cd .deploy && aws ecr put-lifecycle-policy --repository-name "${repoName}" --lifecycle-policy-text file://$(pwd)/conf/lifecycle_policy.json || true`);
-        // info(buffer.toString());
-
-        // info("Deploying...")
-        // let stream = subProcess.exec(`cd .deploy && echo "${process.env.APP_ENV}" | npm run deploy`);
-        // stream.stdout?.on('data', (data) => {
-        //     info(data);
-        // });
-        // stream.stderr?.on('data', (data) => {
-        //     info("stderr: " + data);
-        // });
     } catch (e: any) {
         setFailed(e);
     }
